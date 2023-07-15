bc6f7dc6215a84aec92fbb9ef6a31afb0f97e205
Nhlobo <nhlobo365@gmail.com>
2023-07-14 21:03:52 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the last digit of a randomly generated number
+ * and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int num;
+while (num < 10)
+{
+printf('%d"), num);
+num += 1;
+}
+printf("\n");
+return (0);
+}
