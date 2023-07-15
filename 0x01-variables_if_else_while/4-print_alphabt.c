303d584b4f043e904c229b571bd9631e2602c0ae
Nhlobo <nhlobo365@gmail.com>
2023-07-14 20:56:15 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the last digit of a randomly generated number
+ * and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int lowerCase - 'a';
+while (lowerCase <= 'z')
+{
+lowerCase += 1;
+}
+else
+{
+putchar(lowerCase);
+lowerCase += 1;
+}
+{
+putchar('\n');
+return (0);
+}
