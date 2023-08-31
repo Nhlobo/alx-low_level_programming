47bef8f1cb304380ab0a57b6001c5d157eea5415
Nhlobo <nhlobo365@gmail.com>
2023-08-31 04:36:30 -0700

    101-password

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,16 +1,16 @@
-#include "holberton.h"
-
+#include "main.h"
+#include <stdio.h>
 /**
- *get_endianness - this function checks foe endianness
- *Return: 0 if big endian and 1 if little endian
- */
-
+* get_endianness - function that returns processor endianness
+*
+* Return: 0 if big endian, 1 if little endian
+*/
 int get_endianness(void)
 {
-	unsigned int b = 1;
-	char *a = (char *)&b;
-	if (*a)
-		return (1);
-	else
-		return (0);
+int k;
+char *endian;
+
+k = 1;
+endian = (char *)&k;
+return ((int)endian[0]);
 }
