c9e037beb378ebf9941efcba9991277bda65771b
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2023-12-11 14:34:56 +0200

    Update 4-strpbrk.c

--- 0x18-dynamic_libraries/4-strpbrk.c
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -1,29 +1,32 @@
 #include "main.h"
-#include <stdio.h>
+#define NULL 0
 
 /**
- * *_strpbrk - searches a string for any of a set of bytes
- * @s: string to search
- * @accept: stringcontaining the bytes to look for
- *
- * Return: pointer to the byte in s that matches one of the bytes in accept
- * or NULL if no such byte is found
- */
+* _strpbrk - return pointer to byte in s that matches a byte in accept
+* @s: string to search
+* @accept: target matches
+* Return: pointer to index of string at first occurence
+*/
+
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	int x = 0, y;
 
-	for (i = 0; *s != '\0'; i++)
+	while (s[x] != '\0')
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (y = 0; accept[y] != '\0'; y++)
 		{
-			if (*s == accept[j])
+			if (s[x] == accept[y])
 			{
+				s = &s[x];
 				return (s);
 			}
+
 		}
-		s++;
+
+	x++;
 	}
 
 	return (NULL);
+
 }
