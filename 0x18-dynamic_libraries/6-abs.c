e90230cbb6e9bae3b9680a01892c83a4220bc182
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2023-12-11 14:36:13 +0200

    Update 6-abs.c

--- 0x18-dynamic_libraries/6-abs.c
+++ 0x18-dynamic_libraries/6-abs.c
@@ -1,9 +1,9 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _abs - computes the absolute value of an integer
- * @n: the int to check
- * Return: the absolute value of int
+ * @n: The int to be operated upon
+ * Return: returns an unsigned int value to calling function
  */
 int _abs(int n)
 {
