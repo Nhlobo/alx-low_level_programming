5f41db00f795b0e0a79eac6d9ab95702fb71dcb4
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2023-12-11 14:16:37 +0200

    Update 0-memset.c

--- 0x18-dynamic_libraries/0-memset.c
+++ 0x18-dynamic_libraries/0-memset.c
@@ -1,21 +1,21 @@
 #include "main.h"
-
 /**
- * *_memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
+ *_memset - fills memory with a constant byte.
+ *fills the first n bytes of the memory area pointed
+ *to by s with the constant b.
+ *@s: pointer to memory area.
+ *@b: constant to fill memory with.
+ *@n: bytes of the memory area to be filled.
  *
- * Return: pointer to the memory area s
- */
+ *Return: pointer to memory area s.
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int counter;
 
-	for (i = 0; i < n; i++)
+	for (counter = 0; counter < n; counter++)
 	{
-		s[i] = b;
+		s[counter] = b;
 	}
-
 	return (s);
 }
